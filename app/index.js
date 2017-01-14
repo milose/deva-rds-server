267622bdc140e5deed947561c7a3316b16d16503
Milos Sakovic <m@deva.co>
2017-01-14 02:48:52 +0100

    Add JavaScript standard.

--- app/index.js
+++ app/index.js
@@ -1,31 +1,33 @@
-'use strict';
+'use strict'
 
-exports.start = function(express, port, io, previous) {
-    var app = express();
-    var df = require('dateformat');
+const path = require('path')
 
-    app.set('port', (port || 5000));
+exports.start = function (express, port, io, previous) {
+  var app = express()
+  var df = require('dateformat')
 
-    app.use(express.static(__dirname + '/public'));
+  app.set('port', (port || 5000))
+
+  app.use(express.static(path.join(__dirname, 'public')))
 
     // views is directory for all template files
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
 
-    app.get('/', function(request, response) {
-        var data = {
-            io: io,
-            sockets: io.sockets.sockets,
-            df: df,
-            previous,
-        };
+  app.get('/', function (request, response) {
+    var data = {
+      io: io,
+      sockets: io.sockets.sockets,
+      df: df,
+      previous
+    }
 
         // console.log(require('util').inspect(io.engine.clients, {depth:null}))
 
-        response.render('pages/index', data);
-    });
+    response.render('pages/index', data)
+  })
 
-    app.listen(app.get('port'), function() {
-        console.log('Front-end server is running on port', app.get('port'));
-    });
+  app.listen(app.get('port'), function () {
+    console.log('Front-end server is running on port', app.get('port'))
+  })
 }
--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 'use strict'
 
-require('dotenv').config({
-    path: __dirname + '/.env'
-})
 const fs = require('fs')
 const path = require('path')
+
+require('dotenv').config({
+  path: path.join(__dirname, '.env')
+})
 let request = require('request')
 let express = require('express')
 let chokidar = require('chokidar')
-let S = require('string').extendPrototype()
 let server = require('http').Server(express())
 let io = require('socket.io')(server)
 
@@ -26,125 +26,123 @@ console.log('Web-socket server is running on port', process.env.WS_PORT)
 require('./app/index.js').start(express, process.env.PORT, io, previous)
 
 // Notify slack on start
-notify("Server started: http://deva.co:3338")
+notify('Server started: http://deva.co:3338')
 
 // RDS Loader
-let loadRds = function(file) {
-    let channel = path.dirname(file)
+let loadRds = function (file) {
+  let channel = path.dirname(file)
         .split(process.env.RDS_WATCH)
         .pop()
 
-    let data = fs.readFileSync(file, 'utf8').trim()
+  let data = fs.readFileSync(file, 'utf8').trim()
 
-    if (data.length > 0 && data != previous[channel]) {
-        previous[channel] = data
+  if (data.length > 0 && data !== previous[channel]) {
+    previous[channel] = data
 
-        io.emit(process.env.WS_KEY + '.' + channel, data)
+    io.emit(process.env.WS_KEY + '.' + channel, data)
 
-        if (process.env.RDS_SILENT == 'false') {
-            console.log('Channel:', channel, '| Data:', data)
-        }
+    if (process.env.RDS_SILENT === 'false') {
+      console.log('Channel:', channel, '| Data:', data)
     }
+  }
 }
 
 // Watcher
-let watch_path = __dirname + '/' + process.env.RDS_WATCH
-let watch_pattern = watch_path + '**/*.txt'
+let watchPath = path.join(__dirname, process.env.RDS_WATCH)
+let watchPattern = watchPath + '**/*.txt'
 
-if (process.env.RDS_SILENT == 'false') {
-    console.log('Watching', watch_pattern)
+if (process.env.RDS_SILENT === 'false') {
+  console.log('Watching', watchPattern)
 }
 
-chokidar.watch(watch_pattern, {
-    ignored: /[\/\\]\./,
-    ignoreInitial: true,
+chokidar.watch(watchPattern, {
+  ignored: /[\\]\./,
+  ignoreInitial: true
 }).on('change', loadRds)
 
 // Read all channels on start
-fs.readdirSync(watch_path).forEach(function(dir) {
-    fs.readdirSync(watch_path + dir).forEach(function(file) {
-        if (path.extname(file) == '.txt') {
-            loadRds(watch_path + dir + '/' + file)
-        }
-    })
+fs.readdirSync(watchPath).forEach(function (dir) {
+  fs.readdirSync(watchPath + dir).forEach(function (file) {
+    if (path.extname(file) === '.txt') {
+      loadRds(path.join(watchPath, dir, file))
+    }
+  })
 })
 
 // Middleware
 io.use((socket, next) => {
-    socket.username = socket.request._query['username']
-    next()
+  socket.username = socket.request._query['username']
+  next()
 })
 
 // On connection
 io.on('connection', (socket) => {
-    if (socket.username == '') return
+  if (socket.username === '') return
 
-    let msg = 'Client connected: ' + socket.username + ' / ' + socket.request.connection.remoteAddress
+  let msg = 'Client connected: ' + socket.username + ' / ' + socket.request.connection.remoteAddress
 
-    if (process.env.RDS_SILENT == 'false') {
-        console.log(msg)
-    }
+  if (process.env.RDS_SILENT === 'false') {
+    console.log(msg)
+  }
 
     // Emmit the last message for the reconnected client
-    if (previous[socket.username] != '') {
-        io.emit(process.env.WS_KEY + '.' + socket.username, previous[socket.username])
-    }
+  if (previous[socket.username] !== '') {
+    io.emit(process.env.WS_KEY + '.' + socket.username, previous[socket.username])
+  }
 
     // Reduce Slack spam
-    if (typeof connections[socket.username] != 'undefined') {
-        clearTimeout(connections[socket.username].timeout)
+  if (typeof connections[socket.username] !== 'undefined') {
+    clearTimeout(connections[socket.username].timeout)
+  } else {
+    connections[socket.username] = {
+      shouldNotify: true,
+      timeout: null
     }
-    else {
-        connections[socket.username] = {
-            shouldNotify: true,
-            timeout: null,
-        }
-    }
-    
-    if (connections[socket.username].shouldNotify == true) {
-        notify(msg)
-    }
+  }
 
-    socket.on('disconnect', () => {
-        let msg = 'Client disconnected: ' + socket.username + '!'
+  if (connections[socket.username].shouldNotify === true) {
+    notify(msg)
+  }
 
-        if (process.env.RDS_SILENT == 'false') {
-            console.log(msg)
-        }
+  socket.on('disconnect', () => {
+    let msg = 'Client disconnected: ' + socket.username + '!'
+
+    if (process.env.RDS_SILENT === 'false') {
+      console.log(msg)
+    }
 
         /*
             Create a timeout to reduce spam on Slack
          */
-        connections[socket.username] = {
-            shouldNotify: false,
-            timeout: setTimeout(function() {
-                notify(msg)
-                connections[socket.username].shouldNotify = true
-            }, 15000)
-        }
-
-    })
+    connections[socket.username] = {
+      shouldNotify: false,
+      timeout: setTimeout(function () {
+        notify(msg)
+        connections[socket.username].shouldNotify = true
+      }, 15000)
+    }
+  })
 })
 
 // Slack Notifier
-function notify(text) {
-    if (process.env.SLACK_URL == '') return;
+function notify (text) {
+  if (process.env.SLACK_URL === '') return
 
-    try {
-        request.post(process.env.SLACK_URL, {
-            form: {
-                payload: JSON.stringify({
-                    'username': 'rds.deva.co',
-                    'icon_emoji': ':cloud:',
-                    'text': text,
-                })
-            }
-        });
-    } catch (ex) {
-        var msg = 'Slack error: ' + ex;
+  try {
+    request.post(process.env.SLACK_URL, {
+      form: {
+        payload: JSON.stringify({
+          'username': 'rds.deva.co',
+          'icon_emoji': ':cloud:',
+          'text': text
+        })
+      }
+    })
+  } catch (ex) {
+    var msg = 'Slack error: ' + ex
 
-        if (process.env.RDS_SILENT == 'false') {
-            console.log(msg);
-        }
+    if (process.env.RDS_SILENT === 'false') {
+      console.log(msg)
     }
+  }
 }
